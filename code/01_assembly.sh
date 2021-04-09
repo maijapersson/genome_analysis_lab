9b1d4fff267d3e5ab69e692dae8aa844c97b7444
Maija Persson <mape0100@s21.uppmax.uu.se>
2021-04-09 11:10:36 +0200

    script file for assembly

---
+++ code/01_assembly.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 16:00:00
+#SBATCH -J job_name
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Maija.Persson.0100@student.uu.se
+# Load modules
+module load bioinfo-tools
+module load canu/2.0
+# Your commands
+canu -p assembly -d analyses/01_genome_assembly/ genomeSize=3m -pacbio data/raw_data/DNA_raw_data/ERR2028*
