71cbb85da8cdffe4ef0c315be1588fcb9b56d514
Maija Persson <mape0100@rackham2.uppmax.uu.se>
2021-05-03 15:43:26 +0200

    fixed the prefix of the text file

--- code/09_counting_htseq.sh
+++ code/09_counting_htseq.sh
@@ -13,6 +13,5 @@ module load htseq/0.12.4
 module load samtools
 
 samtools index $1  
-PREFIX=${1%%.sorted*}
 
-htseq-count -f bam -t CDS -i ID -s reverse -r pos $1 analyses/04_mapping_counting/annotation_wo_ns.gff > analyses/04_mapping_counting/$PREFIX_continuous_counting_cds.txt
+htseq-count -f bam -t CDS -i ID -s reverse -r pos $1 analyses/04_mapping_counting/annotation_wo_ns.gff > analyses/04_mapping_counting/$2.batch_counting_cds.txt
