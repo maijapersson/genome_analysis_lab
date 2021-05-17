d71decb0b9fc94e94cb940991a8d00b56b3c8e6c
Maija Persson <mape0100@rackham3.uppmax.uu.se>
2021-05-17 14:42:45 +0200

    added report code

--- code/10_deseq.R
+++ code/10_deseq.R
@@ -21,5 +21,16 @@ res <- results(dds)
 resLFC <- lfcShrink(dds, coef="condition_continuous_vs_batch", type="apeglm")
 
 #Plot both result with and without shrinking
+png("ma_plot.png")
 plotMA(res, ylim=c(-2,2))
+dev.off()
+
+png("ma_plot_shrink.png")
 plotMA(resLFC, ylim=c(-2,2))
+dev.off()
+
+#Creating report
+library(ReportingTools)
+report <- HTMLReport(shortName = 'DiffExp_analysis_with_DESeq2_gene_name', title = 'Differential expression analysis on continous vs batch cultures with gene names', reportDirectory = 'analyses/05_diff_exp')
+publish(dds, report, pvalueCutoff=0.05, make.plots = TRUE, factor = sampleTable$condition, reportDir = "analyses/05_diff_exp")
+finish(report)
